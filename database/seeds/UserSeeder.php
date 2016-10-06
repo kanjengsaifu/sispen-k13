f2418b037af6f72a240d64232e8da412157f706d
nothing628 <yogyphang@gmail.com>
2016-10-07 03:41:42 +0700

    perbaikan user admin

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,6 +13,6 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert(['guru_id' => 1, 'name' => 'test', 'email' => 'test1@test.com', 'password' => bcrypt('123456')]);
         DB::table('users')->insert(['guru_id' => 2, 'name' => 'test2', 'email' => 'test2@test.com', 'password' => bcrypt('123456')]);
-        DB::table('users')->insert(['name' => 'administrator', 'email' => 'admin@admin.com', 'password' => bcrypt('admin')]);
+        DB::table('users')->insert(['name' => 'administrator', 'email' => 'admin@admin.com', 'password' => bcrypt('admin'), 'is_admin' => true]);
     }
 }
