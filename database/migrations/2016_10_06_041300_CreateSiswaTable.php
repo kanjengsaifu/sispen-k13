1d61660ab41b7b735944bb2e7809b5bd580c0648
nothing628 <yogyphang@gmail.com>
2016-10-06 11:19:55 +0700

    add resources

---
+++ database/migrations/2016_10_06_041300_CreateSiswaTable.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->string('nis', 20)->primary();
+            $table->integer('kelas_id');
+            $table->string('nama');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswas');
+    }
+}
